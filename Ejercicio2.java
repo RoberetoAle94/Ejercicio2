46c681f84f13c74f3b664fdbe4b1d3f0562b7b2a
RobertoAle94 <52420574+RobertoAle94@users.noreply.github.com>
2019-07-02 14:49:56 -0500

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio2{
+
+	public static void main (String []args){
+
+		float num, den;
+
+				
+		Scanner lee=new Scanner(System.in);
+
+		System.out.println("Ingrese el numerador1: ");
+
+			num=lee.nextFloat();
+
+		System.out.println("Ingrese el denominador2: ");
+
+			den=lee.nextFloat();
+		}
+}
+
+#modifico Alejandro Andino
